edd69e6738e999d3e6f6a2204d83196c8cd8bcf9
Salahaddin50 <salahaddin665@gmail.com>
2026-01-17 17:14:51 +0300

    Normalize WhatsApp via model mutators

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Scopes\BranchScope;
+use App\Support\WhatsAppNormalizer;
 use Illuminate\Database\Eloquent\Model;
 
 class Driver extends Model
@@ -27,6 +28,11 @@ class Driver extends Model
     {
         parent::boot();
         static::addGlobalScope(new BranchScope());
+    }
+
+    public function setWhatsappAttribute($value): void
+    {
+        $this->attributes['whatsapp'] = WhatsAppNormalizer::normalize($value);
     }
 }
 
--- app/Models/OnlineUser.php
+++ app/Models/OnlineUser.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Scopes\BranchScope;
+use App\Support\WhatsAppNormalizer;
 use Illuminate\Database\Eloquent\Model;
 
 class OnlineUser extends Model
@@ -28,6 +29,11 @@ class OnlineUser extends Model
     {
         parent::boot();
         static::addGlobalScope(new BranchScope());
+    }
+
+    public function setWhatsappAttribute($value): void
+    {
+        $this->attributes['whatsapp'] = WhatsAppNormalizer::normalize($value);
     }
 }
 
